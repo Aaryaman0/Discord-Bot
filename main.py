3a461fe97deed9a746d2f60a20139d6b3c46eb7d
Aaryaman0 <abhageri@uwaterloo.ca>
2023-02-04 22:12:12 -0500

    Added functionality to respond to different users

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents = intents)
 
 @client.event
 async def on_ready():
@@ -11,7 +14,26 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send(':grape:')
+    if message.content.startswith(':grapes:'):
+        await message.channel.send(':grapes:')
+    
+    else:
+        if str(message.author) == "hb#3238":
+            await message.channel.send('Boke')
+        
+        if str(message.author) == "SoaringMonkey13#2781":
+            await message.channel.send('But why?')
 
-client.run(idk)
+        if str(message.author) == "Bearpooh#0625":
+            await message.channel.send('50 can')
+
+        if str(message.author) == "Lenopix#9999":
+            await message.channel.send(':smiley_cat:')
+        
+        if str(message.author) == "Cucumberbatch#2932":
+            await message.channel.send(':clown: Simp')
+        
+        if str(message.author) == "Aaditya#3260":
+            await message.channel.send('PUUHFECT')
+
+client.run('')
