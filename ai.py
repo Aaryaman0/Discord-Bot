0c97dcfbcee0b0cb44d0e77f8fa94b7fa79f3e90
Aaryaman0 <abhageri@uwaterloo.ca>
2023-02-22 23:06:46 -0500

    input prompt and output response

--- ai.py
+++ ai.py
@@ -1,5 +1,11 @@
 import openai
-openai.api_key = "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"
+openai.api_key = ""
 #a = openai.Model.list()
 #print(a)
-print(openai.Model.retrieve("text-davinci-003"))
+#print(openai.Model.retrieve("text-davinci-003"))
+print(openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Insult my friend Hans by saying something about how his beard is weird",
+  max_tokens=50,
+  temperature=0
+)["choices"][0]["text"])
