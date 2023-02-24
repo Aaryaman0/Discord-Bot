6ce90130a45306286f0af6e950103d5613d534a2
Aaryaman0 <abhageri@uwaterloo.ca>
2023-02-23 22:55:46 -0500

    Added basic openai text functionality

--- ai.py
+++ ai.py
@@ -3,9 +3,12 @@ openai.api_key = ""
 #a = openai.Model.list()
 #print(a)
 #print(openai.Model.retrieve("text-davinci-003"))
-print(openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Insult my friend Hans by saying something about how his beard is weird",
-  max_tokens=50,
-  temperature=0
-)["choices"][0]["text"])
+arr = ["beard", "face"]
+
+def insult(friend, feature):
+  return openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Insult my friend" + friend + "by saying something about how his" + feature + "is weird or stupid",
+    max_tokens=50,
+    temperature=0
+  )["choices"][0]["text"]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import openai
+import ai
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,26 +15,31 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    m = message.content
+    
+    if message.content.startswith('@WinstonBot  insult Aaryaman'):
+        await message.channel.send(message.content)
 
-    if message.content.startswith(':grapes:'):
-        await message.channel.send(':grapes:')
+    if message.content.startswith('🍇'):
+        await message.channel.send(message.content)
     
-    if str(message.author) == "hb#3238":
-        await message.channel.send('Boke')
+    #if str(message.author) == "hb#3238":
+    #    await message.channel.send('Boke')
     
-    if str(message.author) == "SoaringMonkey13#2781":
-        await message.channel.send('But why?')
+    #if str(message.author) == "SoaringMonkey13#2781":
+    #    await message.channel.send('But why?')
 
-    if str(message.author) == "Bearpooh#0625":
-        await message.channel.send('50 can')
+    #if str(message.author) == "Bearpooh#0625":
+    #    await message.channel.send('50 can')
 
-    if str(message.author) == "Lenopix#9999":
-        await message.channel.send(':smiley_cat:')
+    #if str(message.author) == "Lenopix#9999":
+    #    await message.channel.send(':smiley_cat:')
     
-    if str(message.author) == "Cucumberbatch#2932":
-        await message.channel.send(':clown: Simp')
+    #if str(message.author) == "Cucumberbatch#2932":
+    #    await message.channel.send(':clown: Simp')
     
-    if str(message.author) == "Aaditya#3260":
-        await message.channel.send('PUUHFECT')
+    #if str(message.author) == "Aaditya#3260":
+    #    await message.channel.send('PUUHFECT')
 
 client.run('')
